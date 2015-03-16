3bd5b9e73304bcd3ef08cd29d877e5957eeceece
Fabià <fabia.serra92@hotmail.com>
2015-03-16 01:46:40 -0700

    bool bgGradient

--- src/ofApp.h
+++ src/ofApp.h
@@ -6,7 +6,7 @@
 #include "ofxKinect.h"
 
 // comment this to use the recorded images
-#define KINECT_CONNECTED
+//#define KINECT_CONNECTED
 
 // Uncomment this to use an xml sequence file for the tracking
 //#define KINECT_SEQUENCE
@@ -157,6 +157,7 @@ class ofApp : public ofBaseApp{
         float guiWidth;
         //--------------------------------------------------------------
         float red, green, blue;
+        bool bgGradient;
         //--------------------------------------------------------------
         float nearClipping, farClipping;
         float nearThreshold, farThreshold;
