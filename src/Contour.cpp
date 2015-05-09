42247af019d94179a4a4a00578d341a61ce8c103
fabiaserra <fabia.serra92@gmail.com>
2015-05-09 02:10:32 -0700

    Add license template to all the classes.

--- src/Contour.cpp
+++ src/Contour.cpp
@@ -1,3 +1,22 @@
+/*
+ * Copyright (C) 2015 Fabia Serra Arrizabalaga
+ *
+ * This file is part of Crea
+ *
+ * Crea is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
 #include "Contour.h"
 
 using namespace ofxCv;
@@ -366,7 +385,7 @@ void Contour::draw(){
         ofSetLineWidth(2.5);
         for(int i = 0; i < diffContours.size(); i++){
             diffContours[i].draw();
-        }
+        }   
         ofPopStyle();
     }
     if(drawDiffImage){
--- src/main.cpp
+++ src/main.cpp
@@ -1,74 +1,27 @@
+/*
+ * Copyright (C) 2015 Fabia Serra Arrizabalaga
+ *
+ * This file is part of Crea
+ *
+ * Crea is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Affero General Public License as published by the Free
+ * Software Foundation (FSF), either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the Affero GNU General Public License
+ * version 3 along with this program.  If not, see http://www.gnu.org/licenses/
+ */
+
 #include "ofMain.h"
 #include "ofApp.h"
-#include "ofGLProgrammableRenderer.h"
 
 //========================================================================
 int main(){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
-//    ofSetCurrentRenderer(ofGLProgrammableRenderer::TYPE);
-//    ofPtr<ofBaseRenderer> renderer(new ofGLProgrammableRenderer(false));
-//    ofSetCurrentRenderer(renderer, false);
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
+	ofSetupOpenGL(1024,768,OF_WINDOW);
 	ofRunApp(new ofApp());
 }
-
-//#include "ofMain.h"
-//#include "ofApp.h"
-//#include "ofAppGLFWWindow.h"
-
-////========================================================================
-//int main( ){
-//    ofGLFWWindowSettings settings;
-//    settings.width = 1024;
-//    settings.height = 768;
-//    settings.setPosition(ofVec2f(300,0));
-//    settings.resizable = true;
-//    shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
-//    
-//    settings.width = 1024;
-//    settings.height = 768;
-//    settings.setPosition(ofVec2f(0,0));
-//    settings.resizable = false;
-//    shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(settings);
-//    guiWindow->setVerticalSync(false);
-//    
-//    shared_ptr<ofApp> mainApp(new ofApp);
-////    mainApp->setupGUI();
-////    ofAddListener(guiWindow->events().draw,mainApp.get(),&ofApp::drawGui);
-//    
-//    ofRunApp(mainWindow, mainApp);
-//    ofRunMainLoop();
-//}
-
-//#include "ofMain.h"
-//#include "ofApp.h"
-//#include "GuiApp.h"
-//#include "ofAppGLFWWindow.h"
-//
-////========================================================================
-//int main( ){
-//    ofGLFWWindowSettings settings;
-//    
-//    settings.width = 600;
-//    settings.height = 600;
-//    settings.setPosition(ofVec2f(300,0));
-//    settings.resizable = true;
-//    shared_ptr<ofAppBaseWindow> mainWindow = ofCreateWindow(settings);
-//    
-//    settings.width = 300;
-//    settings.height = 300;
-//    settings.setPosition(ofVec2f(0,0));
-//    settings.resizable = false;
-//    shared_ptr<ofAppBaseWindow> guiWindow = ofCreateWindow(settings);
-//    
-//    shared_ptr<ofApp> mainApp(new ofApp);
-//    shared_ptr<GuiApp> guiApp(new GuiApp);
-//    mainApp->gui = guiApp;
-//    
-//    ofRunApp(guiWindow, guiApp);
-//    ofRunApp(mainWindow, mainApp);
-//    ofRunMainLoop();
-//    
-//}
